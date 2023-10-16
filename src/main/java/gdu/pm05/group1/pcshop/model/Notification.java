28adf1b77898b61960c972d87f375fa9e194a4ff
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-17 01:42:52 +0700

    Annotate Notification entity class.

--- src/main/java/gdu/pm05/group1/pcshop/model/Notification.java
+++ src/main/java/gdu/pm05/group1/pcshop/model/Notification.java
@@ -3,22 +3,36 @@ package gdu.pm05.group1.pcshop.model;
 import java.util.Date;
 import java.util.List;
 
-// - id: int
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-// - date: Date
-
-// - title: String
-
-// - content: String
-
-// - users: List<User>
-
+@Entity (name = "Notification")
+@Table (name = "Notification")
 public class Notification {
     // FIELDS:
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column (name = "date", nullable = false)
     private Date date;
+
+    @Column (name = "title", nullable = false)
     private String title;
+
+    @Column (name = "content", nullable = false)
     private String content;
+
+    @OneToMany (
+        fetch = FetchType.EAGER,
+        mappedBy = "notification"
+    )
     private List<UserNotification> users;
 
     // CONSTRUCTORS:
