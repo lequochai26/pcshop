31c3cb7a4b59ada9d67d83ef903ce2561da6db81
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-17 11:38:38 +0700

    IDBHandler interface added

---
+++ src/main/java/gdu/pm05/group1/pcshop/model/IDBHandler.java
@@ -0,0 +1,11 @@
+package gdu.pm05.group1.pcshop.model;
+
+import java.util.List;
+
+public interface IDBHandler {
+    void save(Object... objects);
+    void update(Object... objects);
+    void remove(Object... objects);
+    <T> T get(Class<T> objClass, Object... parameters);
+    <T> List<T> getAll();
+}
