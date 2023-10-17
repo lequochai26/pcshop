293421961e837333746e724cac6ea3430ba31f2a
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-17 09:54:57 +0700

    Annotated ItemType entity class

--- src/main/java/gdu/pm05/group1/pcshop/model/ItemType.java
+++ src/main/java/gdu/pm05/group1/pcshop/model/ItemType.java
@@ -2,15 +2,25 @@ package gdu.pm05.group1.pcshop.model;
 
 import java.util.List;
 
-/*- id: String
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-- name: String
-
-- items: List<Item> */
+@Entity (name = "ItemType")
+@Table (name = "ItemType")
 public class ItemType {
     // FIELDS:
+    @Id
+    @Column (name = "id")
     private String id;
+
+    @Column (name = "name", nullable = false)
     private String name;
+
+    @OneToMany (fetch = FetchType.EAGER, mappedBy = "type")
     private List<Item> items;
 
     // CONSTRUCTORS:
