f1730855722994d016caf396e4990fc44f7c81ca
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-17 20:46:05 +0700

    Modified DBHandler interface and added DB Components.

--- src/main/java/gdu/pm05/group1/pcshop/model/IDBHandler.java
+++ src/main/java/gdu/pm05/group1/pcshop/model/IDBHandler.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 public interface IDBHandler {
     void save(Object... objects);
-    void update(Object... objects);
     void remove(Object... objects);
-    <T> T get(Class<T> objClass, Object target);
-    <T> List<T> getAll();
+    <T> T get(Class<T> objClass, T target);
+    <T> List<T> getAll(Class<T> objClass);
 }
