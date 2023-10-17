6a9971ffa602a0f1feb9a7b0648d14e88d4e7ab8
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-18 01:37:01 +0700

    RegisterServlet doPost register feature making.

--- src/main/java/gdu/pm05/group1/pcshop/controller/RegisterServlet.java
+++ src/main/java/gdu/pm05/group1/pcshop/controller/RegisterServlet.java
@@ -2,6 +2,13 @@ package gdu.pm05.group1.pcshop.controller;
 
 import java.io.IOException;
 
+import gdu.pm05.group1.pcshop.model.Cart;
+import gdu.pm05.group1.pcshop.model.User;
+import gdu.pm05.group1.pcshop.model.UserInfo;
+import gdu.pm05.group1.pcshop.model.dbhandler.DBHandler;
+import gdu.pm05.group1.pcshop.model.dbhandler.HQLParameter;
+import gdu.pm05.group1.pcshop.model.dbhandler.IDBHandler;
+import gdu.pm05.group1.pcshop.model.enums.UserPermission;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,9 +18,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet (name="register", urlPatterns="/register")
 public class RegisterServlet extends HttpServlet {
+    // FIELDS:
+    private IDBHandler dbHandler;
+
     // CONSTRUCTORS:
     public RegisterServlet() {
         super();
+        this.dbHandler = DBHandler.getInstance();
     }
 
     // METHODS:
@@ -28,6 +39,80 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.doGet(request, response);
+        // Get parameters from request
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String fullName = request.getParameter("fullName");
+        String genderStr = request.getParameter("sex");
+        Boolean gender = (
+            (genderStr != null)
+            ? genderStr.equals("Male")
+            : true
+        );
+        String phoneNumbers = request.getParameter("phoneNumber");
+        String address = request.getParameter("address");
+
+        // Validate phoneNumbers
+        if (!phoneNumbers.matches("^0\\d{10}$")) {
+            request.setAttribute("message", "Số điện thoại không hợp lệ!");
+            this.doGet(request, response);
+            return;
+        }
+        
+        // Get user from db with given username
+        User user = dbHandler.get(
+            User.class,
+            new HQLParameter("username", username)
+        );
+
+        // User already exist case
+        if (user != null) {
+            request.setAttribute(
+                "message",
+                "Người dùng '@username' đã tồn tại!".replace("@username", username)
+            );
+            this.doGet(request, response);
+            return;
+        }
+
+        // Create new user
+        user = new User();
+        UserInfo userInfo = new UserInfo();
+        Cart cart = new Cart();
+
+        // Set user's informations
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setPermission(UserPermission.CUSTOMER);
+        user.setUserInfo(userInfo);
+        user.setCart(cart);
+
+        // Set userInfo's informations
+        userInfo.setUsername(username);
+        userInfo.setFullName(fullName);
+        userInfo.setGender(gender);
+        userInfo.setPhoneNumbers(phoneNumbers);
+        userInfo.setAddress(address);
+        userInfo.setUser(user);
+
+        // Set cart's informations
+        cart.setUsername(username);
+        cart.setUser(user);
+
+        // Save all data related to user to database
+        dbHandler.save(user, userInfo, cart);
+
+        // Set attributes for request
+        request.setAttribute("color", "green");
+        request.setAttribute(
+            "message",
+            "Đăng ký tài khoản thành công!"
+        );
+
+        // Get dispatcher
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/message.jsp");
+
+        // Forward
+        dispatcher.forward(request, response);
     }
 }
