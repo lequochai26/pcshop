7a4922e421f7e987bd026bb129a7d2a4bdbab00a
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-18 19:02:17 +0700

    AdministratorServlet initialized

---
+++ src/main/java/gdu/pm05/group1/pcshop/controller/AdministratorServlet.java
@@ -0,0 +1,33 @@
+package gdu.pm05.group1.pcshop.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet (name = "administrator", urlPatterns = "/administrator")
+public class AdministratorServlet extends HttpServlet {
+    // CONSTRUCTORS:
+    public AdministratorServlet() {
+
+    }
+
+    // METHODS:
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get dispatcher
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/administrator.jsp");
+
+        // Forward
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
