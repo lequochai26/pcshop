28a0007a037a08860f218bd8c49438532f6be3f6
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-19 00:12:17 +0700

    Added validationResult parameter for validate method of Validator.

--- src/main/java/gdu/pm05/group1/pcshop/model/validator/Validator.java
+++ src/main/java/gdu/pm05/group1/pcshop/model/validator/Validator.java
@@ -1,5 +1,7 @@
 package gdu.pm05.group1.pcshop.model.validator;
 
+import java.util.Map;
+
 public interface Validator {
-    void validate(Object... params);
+    void validate(Map<String, Object> validationResult, Object... params);
 }
