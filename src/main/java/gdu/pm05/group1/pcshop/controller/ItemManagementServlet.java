a2b14a2e8bafc4772fed3918d111f18cc5e46f30
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-19 20:16:15 +0700

    Added ItemManagementServlet

---
+++ src/main/java/gdu/pm05/group1/pcshop/controller/ItemManagementServlet.java
@@ -0,0 +1,44 @@
+package gdu.pm05.group1.pcshop.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet (name = "itemmanagement", urlPatterns = "/itemmanagement")
+public class ItemManagementServlet extends AdministratorServlet {
+    // CONSTRUCTORS:
+    public ItemManagementServlet() {
+        super();
+    }
+
+    // METHODS:
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Validation
+        boolean valid = this.validateAdministrator(request, response);
+
+        // Exit if not valid
+        if (!valid) {
+            return;
+        }
+
+        // VALIDATION SUCCESSFULLY
+        // Get request dispatcher
+        RequestDispatcher dispatcher = request.getRequestDispatcher(
+            "WEB-INF/jsp/itemmanagement.jsp"
+        );
+
+        // Forward
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
