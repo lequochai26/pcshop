a6612d72bba603a52845f7f3e4aa21d10c04cf5d
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-21 13:15:56 +0700

    Added validation step and added users attribute for request.

--- src/main/java/gdu/pm05/group1/pcshop/controller/UsersManagementServlet.java
+++ src/main/java/gdu/pm05/group1/pcshop/controller/UsersManagementServlet.java
@@ -1,8 +1,12 @@
 package gdu.pm05.group1.pcshop.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import gdu.pm05.group1.pcshop.model.User;
+import gdu.pm05.group1.pcshop.model.dbhandler.IDBHandler;
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +23,26 @@ public class UsersManagementServlet extends AdministratorServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // Administrator validation
+        boolean valid = this.validateAdministrator(request, response);
+
+        // Exit of not valid
+        if (!valid) {
+            return;
+        }
+
+        // Get context
+        ServletContext context = request.getServletContext();
+
+        // Get dbHandler
+        IDBHandler dbHandler = (IDBHandler)context.getAttribute("dbHandler");
+
+        // Get all users in db
+        List<User> users = dbHandler.getAll(User.class);
+
+        // Set users attribute for request
+        request.setAttribute("users", users);
+        
         // Get request dispatcher
         RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/usersmanagement.jsp");
 
