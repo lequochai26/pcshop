ab2edb41f46b244b1c58955d0f80ca8cb80d108e
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-21 21:13:55 +0700

    Added administrator validation and load all orders from DB.

--- src/main/java/gdu/pm05/group1/pcshop/controller/OrdersManagementServlet.java
+++ src/main/java/gdu/pm05/group1/pcshop/controller/OrdersManagementServlet.java
@@ -1,8 +1,12 @@
 package gdu.pm05.group1.pcshop.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import gdu.pm05.group1.pcshop.model.Order;
+import gdu.pm05.group1.pcshop.model.dbhandler.IDBHandler;
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +23,26 @@ public class OrdersManagementServlet extends AdministratorServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // Administrator validation
+        boolean valid = this.validateAdministrator(request, response);
+
+        // Exit if not valid
+        if (!valid) {
+            return;
+        }
+
+        // Get context
+        ServletContext context = request.getServletContext();
+
+        // Get DBHandler
+        IDBHandler dbHandler = (IDBHandler)context.getAttribute("dbHandler");
+
+        // Get all orders
+        List<Order> orders = dbHandler.getAll(Order.class);
+
+        // Set orders attribute for request
+        request.setAttribute("orders", orders);
+
         // Get dispatcher
         RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/ordersmanagement.jsp");
 
