166095640ac9804785e6165b4c91a520a38d5d5f
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-22 02:15:43 +0700

    Added result for administrator validation enum and added administrator validate method for ServletUtil

--- src/main/java/gdu/pm05/group1/pcshop/controller/util/ServletUtil.java
+++ src/main/java/gdu/pm05/group1/pcshop/controller/util/ServletUtil.java
@@ -5,9 +5,11 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import gdu.pm05.group1.pcshop.controller.util.enums.AdministratorValidationResult;
 import gdu.pm05.group1.pcshop.controller.util.enums.UserValidationResult;
 import gdu.pm05.group1.pcshop.model.User;
 import gdu.pm05.group1.pcshop.model.dbhandler.IDBHandler;
+import gdu.pm05.group1.pcshop.model.enums.UserPermission;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +18,68 @@ import jakarta.servlet.http.Part;
 
 public class ServletUtil {
     // STATIC METHODS:
+    public static Map<String, Object> administratorValidate(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) {
+        return administratorValidate(request, response, true);
+    }
+
+    public static Map<String, Object> administratorValidate(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        boolean logout
+    ) {
+        // Path initialization
+        Map<String, Object> path = new HashMap<>();
+
+        // Administrator validation
+        administratorValidate(request, response, logout, path);
+
+        // Return path
+        return path;
+    }
+
+    public static void administratorValidate(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Map<String, Object> path
+    ) {
+        administratorValidate(request, response, true, path);
+    }
+
+    public static void administratorValidate(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        boolean logout,
+        Map<String, Object> path
+    ) {
+        // User validation
+        userValidate(request, response, logout, path);
+
+        // Get userValidateResult from path
+        UserValidationResult userValidateResult = (UserValidationResult)path.get("userValidateResult");
+
+        // User validation failed case
+        if (userValidateResult != UserValidationResult.SUCCESSFULLY) {
+            path.put("administratorValidateResult", AdministratorValidationResult.USER_VALIDATION_FAILED);
+            return;
+        }
+
+        // User validation successfully case
+        // Get user entity from path
+        User user = (User)path.get("user");
+
+        // Check user's permission
+        if (user.getPermission() != UserPermission.ADMIN) {
+            path.put("administratorValidateResult", AdministratorValidationResult.NOT_ADMINISTRATOR);
+            return;
+        }
+
+        // Administrator validation successfully
+        path.put("administratorValidateResult", AdministratorValidationResult.IS_ADMINISTRATOR);
+    }
+
     /**
      * 
      * @param request Servlet request
@@ -69,7 +133,7 @@ public class ServletUtil {
         HttpServletResponse response,
         Map<String, Object> path
     ) {
-        userValidate(request, response, false, path);
+        userValidate(request, response, true, path);
     }
 
     /**
