38a3d71e2f08a8de0288321b0c8d4108353ae282
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-22 13:23:07 +0700

    Added load item data for ItemDetailServlet

--- src/main/java/gdu/pm05/group1/pcshop/controller/ItemDetailServlet.java
+++ src/main/java/gdu/pm05/group1/pcshop/controller/ItemDetailServlet.java
@@ -2,7 +2,12 @@ package gdu.pm05.group1.pcshop.controller;
 
 import java.io.IOException;
 
+import gdu.pm05.group1.pcshop.controller.util.ServletUtil;
+import gdu.pm05.group1.pcshop.model.Item;
+import gdu.pm05.group1.pcshop.model.dbhandler.HQLParameter;
+import gdu.pm05.group1.pcshop.model.dbhandler.IDBHandler;
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -19,6 +24,33 @@ public class ItemDetailServlet extends HttpServlet {
     // METHODS:
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get id parameter
+        String id = request.getParameter("id");
+
+        // ID Null case
+        if (id == null) {
+            ServletUtil.showInputRequiredMessage(request, response);
+            return;
+        }
+
+        // Get context
+        ServletContext context = request.getServletContext();
+
+        // Get DBHandler
+        IDBHandler dbHandler = (IDBHandler)context.getAttribute("dbHandler");
+
+        // Get Item entity with given id
+        Item item = dbHandler.get(Item.class, new HQLParameter("id", id));
+
+        // Item not exist case
+        if (item == null) {
+            ServletUtil.showDataNotExistMessage(request, response);
+            return;
+        }
+
+        // Set item attribute for request
+        request.setAttribute("item", item);
+
         // Get dispatcher
         RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/itemdetail.jsp");
 
