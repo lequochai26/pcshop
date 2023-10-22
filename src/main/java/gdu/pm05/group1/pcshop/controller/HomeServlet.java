c9bd29a56eb87ead9c782a2718c36eba1bcf070b
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-22 23:58:26 +0700

    Rebuilt HomeServlet

--- src/main/java/gdu/pm05/group1/pcshop/controller/HomeServlet.java
+++ src/main/java/gdu/pm05/group1/pcshop/controller/HomeServlet.java
@@ -1,11 +1,12 @@
 package gdu.pm05.group1.pcshop.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import gdu.pm05.group1.pcshop.model.validator.UserValidator;
-import gdu.pm05.group1.pcshop.model.validator.Validator;
+import gdu.pm05.group1.pcshop.controller.util.ServletUtil;
+import gdu.pm05.group1.pcshop.model.Item;
+import gdu.pm05.group1.pcshop.model.dbhandler.IDBHandler;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,39 +16,38 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet (name = "home", urlPatterns = "/home")
 public class HomeServlet extends HttpServlet {
-    // FIELDS:
-    private Validator userValidator;
-
     // CONSTRUCTORS:
     public HomeServlet() {
         super();
-        userValidator = UserValidator.getInstance();
     }
 
     // METHODS:
     @Override
-    protected void doGet(
-        HttpServletRequest request,
-        HttpServletResponse response
-    ) throws ServletException, IOException {
-        // Create a validation result map
-        Map<String, Object> validationResult = new HashMap<>();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // User validation
+        Map<String, Object> path = ServletUtil.userValidate(request, response);
 
-        // User validating
-        userValidator.validate(validationResult, request);
+        // Get DBHandler
+        IDBHandler dbHandler = (IDBHandler)path.get("dbHandler");
+
+        // If request's items property is null
+        if (request.getAttribute("items") == null) {
+            // Get all Item entities in db
+            List<Item> items = dbHandler.getAll(Item.class);
+
+            // Set items attribute for request
+            request.setAttribute("items", items);
+        }
 
         // Get dispatcher
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/homepage.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/homepage.jsp");
 
         // Forward
         dispatcher.forward(request, response);
     }
 
     @Override
-    protected void doPost(
-        HttpServletRequest request,
-        HttpServletResponse response
-    ) throws ServletException, IOException {
-        this.doGet(request, response);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
     }
 }
