4d447bca2e6c2eeddc64f8616e51f64835c8bd9f
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-22 18:26:49 +0700

    Added load cart items feature for CartServlet

--- src/main/java/gdu/pm05/group1/pcshop/controller/CartServlet.java
+++ src/main/java/gdu/pm05/group1/pcshop/controller/CartServlet.java
@@ -1,13 +1,18 @@
 package gdu.pm05.group1.pcshop.controller;
 
 import java.io.IOException;
+import java.util.Set;
 
+import gdu.pm05.group1.pcshop.model.Cart;
+import gdu.pm05.group1.pcshop.model.CartItem;
+import gdu.pm05.group1.pcshop.model.User;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet (name = "cart", urlPatterns = "/cart")
 public class CartServlet extends HttpServlet {
@@ -19,6 +24,31 @@ public class CartServlet extends HttpServlet {
     // METHODS:
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get session
+        HttpSession session = request.getSession(true);
+
+        // Get User from session
+        User user = (User)session.getAttribute("user");
+
+        // Get cart
+        Cart cart = null;
+        if (user == null) {
+            cart = (Cart)session.getAttribute("cart");
+            if (cart == null) {
+                cart = new Cart();
+                session.setAttribute("cart", cart);
+            }
+        }
+        else {
+            cart = user.getCart();
+        }
+
+        // Get cart's cart items
+        Set<CartItem> cartItems = cart.getItems();
+
+        // Set cartItems attribute for request
+        request.setAttribute("cartItems", cartItems);
+
         // Get dispatcher
         RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/cart.jsp");
 
