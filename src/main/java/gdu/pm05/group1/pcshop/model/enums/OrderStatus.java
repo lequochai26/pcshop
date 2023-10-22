5bf7e308718509b254f750cf25436938679f07e1
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-22 08:38:48 +0700

    Added CANCELLED value for OrderStatus enum.

--- src/main/java/gdu/pm05/group1/pcshop/model/enums/OrderStatus.java
+++ src/main/java/gdu/pm05/group1/pcshop/model/enums/OrderStatus.java
@@ -3,5 +3,6 @@ package gdu.pm05.group1.pcshop.model.enums;
 public enum OrderStatus {
     AWAITING_CONFIRMATION,
     DELIVERING,
-    DELIVERED_SUCCESSFULLY
+    DELIVERED_SUCCESSFULLY,
+    CANCELLED
 }
