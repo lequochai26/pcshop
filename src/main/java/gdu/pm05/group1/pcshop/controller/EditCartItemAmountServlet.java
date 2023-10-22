2a085f18769816b31db7e61a633e738de92af211
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-22 19:57:43 +0700

    Finished EditCartItemAmountServlet

--- src/main/java/gdu/pm05/group1/pcshop/controller/EditCartItemAmountServlet.java
+++ src/main/java/gdu/pm05/group1/pcshop/controller/EditCartItemAmountServlet.java
@@ -1,16 +1,20 @@
 package gdu.pm05.group1.pcshop.controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import gdu.pm05.group1.pcshop.controller.util.ServletUtil;
+import gdu.pm05.group1.pcshop.controller.util.enums.GetCartResult;
 import gdu.pm05.group1.pcshop.model.Cart;
+import gdu.pm05.group1.pcshop.model.CartItem;
 import gdu.pm05.group1.pcshop.model.User;
+import gdu.pm05.group1.pcshop.model.dbhandler.IDBHandler;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @WebServlet (name = "editcartitemamount", urlPatterns = "/editcartitemamount")
 public class EditCartItemAmountServlet extends HttpServlet {
@@ -51,31 +55,70 @@ public class EditCartItemAmountServlet extends HttpServlet {
             return;
         }
 
-        // Get session
-        HttpSession session = request.getSession(false);
+        // Get cart
+        Map<String, Object> path = ServletUtil.getCart(request, response, false);
 
-        // Session null case
-        if (session == null) {
+        // Get result
+        GetCartResult result = (GetCartResult)path.get("getCartResult");
+
+        // Not found case
+        if (result == GetCartResult.NOT_FOUND) {
             ServletUtil.showMessage(
                 request, response,
-                "Không tìm thấy giỏ hàng, vui lòng thử lại!"
+                "Không tìm thấy giỏ hàng!"
             );
+            return;
         }
 
-        // Get user from session
-        User user = (User)session.getAttribute("user");
+        // Get context
+        ServletContext context = request.getServletContext();
+
+        // Get DBHandler
+        IDBHandler dbHandler = (IDBHandler)context.getAttribute("dbHandler");
 
         // Get cart
-        Cart cart = null;
-        if (user == null) {
-            cart = (Cart)session.getAttribute("cart");
-        }
-        else {
-            cart = user.getCart();
+        Cart cart = (Cart)path.get("cart");
+
+        // Edit cart item amount
+        for (CartItem cartItem : cart.getItems()) {
+            // Target cart item detect
+            if (cartItem.getItem().getId().equals(id)) {
+                // Edit amount
+                cartItem.setAmount(
+                    cartItem.getAmount() + amount
+                );
+
+                // Amount less than 1 case
+                if (cartItem.getAmount() < 1) {
+                    if (result == GetCartResult.USER_CART_FOUND) {
+                        dbHandler.remove(cartItem);
+                    }
+                    else {
+                        cart.getItems().remove(cartItem);
+                    }
+                }
+                // Otherwise
+                else {
+                    if (result == GetCartResult.USER_CART_FOUND) {
+                        dbHandler.save(cartItem);
+                    }
+                }
+                
+                break;
+            }
         }
 
-        // Cart null case
-        
+        // Refresh user if neccessary
+        if (result == GetCartResult.USER_CART_FOUND) {
+            // Get user from path
+            User user = (User)path.get("user");
+
+            // Refresh user
+            dbHandler.refresh(user);
+        }
+
+        // Send redirect back to cart page
+        response.sendRedirect("cart");
     }
 
     @Override
